9b8aea71b51a8c4131e806ccd3337b5daa09bd87
Lukas Malek <malek.luky@gmail.com>
2022-04-22 17:22:00 +0200

    Part III Final

--- lukas_functions.py
+++ lukas_functions.py
@@ -9,7 +9,6 @@ from sklearn.preprocessing import LabelEncoder, Normalizer, StandardScaler, labe
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, roc_auc_score
 import sklearn.metrics
-import statsmodels.api as sm
 import plotly.express as px  # for plotting the scatter plot
 import seaborn as sns  # For plotting the dataset in seaborn
 from sklearn.neighbors import KNeighborsClassifier
@@ -20,6 +19,7 @@ from sklearn import model_selection
 from sklearn.model_selection import cross_val_score, KFold
 from sklearn.metrics import classification_report
 import copy
+import scipy
 sns.set(style='whitegrid')
 warnings.filterwarnings('ignore')
 
@@ -354,3 +354,31 @@ def savefig_regularization(lambda_interval, train_error_rate, test_error_rate, o
     plt.title('Parameter vector L2 norm')
     plt.grid()
     plt.savefig("images/lukas/regression/RegularizationL2_"+str(index)+".pdf")
+
+def mcnemar(y_true, yhatA, yhatB, alpha=0.05):
+    # perform McNemars test
+    nn = np.zeros((2,2))
+    c1 = yhatA - y_true == 0
+    c2 = yhatB - y_true == 0
+    nn[0,0] = sum(c1 & c2)
+    nn[0,1] = sum(c1 & ~c2)
+    nn[1,0] = sum(~c1 & c2)
+    nn[1,1] = sum(~c1 & ~c2)
+    n = sum(nn.flat)
+    n12 = nn[0,1]
+    n21 = nn[1,0]
+    thetahat = (n12-n21)/n
+    Etheta = thetahat
+    Q = n**2 * (n+1) * (Etheta+1) * (1-Etheta) / ( (n*(n12+n21) - (n12-n21)**2) )
+    p = (Etheta + 1)*0.5 * (Q-1)
+    q = (1-Etheta)*0.5 * (Q-1)
+    CI = tuple(lm * 2 - 1 for lm in scipy.stats.beta.interval(1-alpha, a=p, b=q) )
+    p = 2*scipy.stats.binom.cdf(min([n12,n21]), n=n12+n21, p=0.5)
+    # print("Result of McNemars test using alpha=", alpha)
+    # print("Comparison matrix n")
+    # print(nn)
+    # if n12+n21 <= 10:
+    #     print("Warning, n12+n21 is low: n12+n21=",(n12+n21))
+    # print("Approximate 1-alpha confidence interval of theta: [thetaL,thetaU] = ", CI)
+    # print("p-value for two-sided test A and B have same accuracy (exact binomial test): p=", p)
+    return [thetahat, CI[0],CI[1], p]
--- lukas_part2.py
+++ lukas_part2.py
@@ -9,7 +9,6 @@ from sklearn.preprocessing import LabelEncoder, Normalizer, StandardScaler, labe
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, roc_auc_score
 import sklearn.metrics
-import statsmodels.api as sm
 import plotly.express as px  # for plotting the scatter plot
 import seaborn as sns  # For plotting the dataset in seaborn
 from sklearn.neighbors import KNeighborsClassifier
@@ -114,6 +113,7 @@ for train_index, test_index in kf.split(data_x):
     index += 1
     
     
+    
     # VOLUNTARY
     # just for double check how the other models behave
     # otherwise we choose only the best model from inner loop
@@ -142,6 +142,87 @@ for train_index, test_index in kf.split(data_x):
     y_prediction5 = np.ones(len(x_test), dtype=int) # 168x male, 165xfemale
     error_baseline.append((1-accuracy_score(y_test, y_prediction5)))
 
+    # STATISTICAL TESTING
+    alpha = 0.05
+    theta1,theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,theta10 = [],[],[],[],[],[],[],[],[],[]
+    p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 = [],[],[],[],[],[],[],[],[],[]
+    CIL1,CIL2,CIL3,CIL4,CIL5,CIL6,CIL7,CIL8,CIL9,CIL10 = [],[],[],[],[],[],[],[],[],[]
+    CIR1, CIR2, CIR3, CIR4, CIR5, CIR6, CIR7, CIR8, CIR9, CIR10 = [
+    ], [], [], [], [], [], [], [], [], []
+
+    stat_test = []
+    #[thetahat, CI, p] = mcnemar(y_true, yhat1, yhat2, alpha=alpha)
+    
+    # KNN - Baseline 11
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction1,y_prediction5, alpha=alpha)
+    theta1.append(theta)
+    CIL1.append(CIL)
+    CIR1.append(CIR)
+    p1.append(p)
+
+    # TREE - Baseline 2
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction2, y_prediction5, alpha=alpha)
+    theta2.append(theta)
+    CIL2.append(CIL)
+    CIR2.append(CIR)
+    p2.append(p)
+
+    # SVM - Baseline 3
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction3, y_prediction5, alpha=alpha)
+    theta3.append(theta)
+    CIL3.append(CIL)
+    CIR3.append(CIR)
+    p3.append(p)
+
+    # LOG - Baseline 4
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction4, y_prediction5, alpha=alpha)
+    theta4.append(theta)
+    CIL4.append(CIL)
+    CIR4.append(CIR)
+    p4.append(p)
+
+    # # KNN - LOG 5
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction1, y_prediction4, alpha=alpha)
+    theta5.append(theta)
+    CIL5.append(CIL)
+    CIR5.append(CIR)
+    p5.append(p)
+
+    # # TREE - LOG 6
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction2, y_prediction4, alpha=alpha)
+    theta6.append(theta)
+    CIL6.append(CIL)
+    CIR6.append(CIR)
+    p6.append(p)
+
+    # # SVM - LOG 7
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction3, y_prediction4, alpha=alpha)
+    theta7.append(theta)
+    CIL7.append(CIL)
+    CIR7.append(CIR)
+    p7.append(p)
+
+    # # KNN - SVM 8
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction1, y_prediction3, alpha=alpha)
+    theta8.append(theta)
+    CIL8.append(CIL)
+    CIR8.append(CIR)
+    p8.append(p)
+    
+    # # TREE - SVM 9
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction2, y_prediction3, alpha=alpha)
+    theta9.append(theta)
+    CIL9.append(CIL)
+    CIR9.append(CIR)
+    p9.append(p)
+
+    # # KNN - TREE 10
+    theta, CIL, CIR, p = mcnemar(y_test, y_prediction1, y_prediction2, alpha=alpha)
+    theta10.append(theta)
+    CIL10.append(CIL)
+    CIR10.append(CIR)
+    p10.append(p)
+
 print("\nOUTER LOOP")
 print("Error Etest KNN:                 [%]", (100*np.array(error_knn)).round(2))
 print("Error Etest Decision Tree:       [%]", (100*np.array(error_decisiontree)).round(2))
@@ -149,10 +230,53 @@ print("Error Etest SVM:                 [%]", (100*np.array(error_svm)).round(2)
 print("Error Etest Logistic Regression: [%]", (100*np.array(error_logisticregression)).round(2))
 print("Error Etest Baseline:            [%]", (100*np.array(error_baseline)).round(2),'\n')
 
+print("STATISTICAL TESTING")
+
+print("KNN - Baseline")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),"\nCI: ", np.mean(CIL1), np.mean(CIR1), "\np-value", np.mean(p1))
+
+print("\nTREE - Baseline")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL2), np.mean(CIR2), "\np-value", np.mean(p2))
+
+print("\nSVM - Baseline")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL3), np.mean(CIR3), "\np-value", np.mean(p3))
+
+print("\nLOG - Baseline")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL4), np.mean(CIR4), "\np-value", np.mean(p4))
+
+print("\nKNN - LOG")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL5), np.mean(CIR5), "\np-value", np.mean(p5))
+
+print("\nTREE - LOG")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL6), np.mean(CIR6), "\np-value", np.mean(p6))
+
+print("\nSVM - LOG")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL7), np.mean(CIR7), "\np-value", np.mean(p7))
+
+print("\nKNN - SVM")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL8), np.mean(CIR8), "\np-value", np.mean(p8))
+
+print("\nTREE - SVM")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL9), np.mean(CIR9), "\np-value", np.mean(p9))
+
+print("\nKNN - TREE")
+print("theta = theta_A-theta_B point estimate", np.mean(theta1),
+      "\nCI: ", np.mean(CIL10), np.mean(CIR10), "\np-value", np.mean(p10))
+
 print("\nSUMMARY")
 print("Error Inner Loop Best Model:     [%]", (100*np.array(error_outloop)).round(2))
 print("Regression Parameter:            [-]", (np.array(regression_par)).round(2))
 print("Total Generalization Error Estimate: ",100*gen_error.round(5),'%\n')
+
+
 
 # # REPORT
 # # print_report(y_test, y_prediction1, y_prediction2,
